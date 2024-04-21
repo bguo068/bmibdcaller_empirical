3443786ba540d3a5d0d220c7c471b5ff4b2c20a3
Bing Guo <gbinux@gmail.com>
2024-04-21 10:08:03 -0400

    add peak_validate_meth and ibdne_no_diploid_conversion options; and refactor

--- bin/proc_dist_ne.py
+++ bin/proc_dist_ne.py
@@ -15,18 +15,25 @@ pa.add_argument("--ibd_files_true", type=str, nargs=14, default=[])
 pa.add_argument("--vcf_files", type=str, nargs=14, required=True)
 pa.add_argument("--genome_set_id", type=str, required=True)
 pa.add_argument("--ibdne_jar", type=str, default=ibd_jar_default)
-pa.add_argument("--ibdne_mincm", type=float, default=2)
+pa.add_argument("--ibdne_mincm", type=float, default=4)
 pa.add_argument("--ibdne_minregion", type=float, default=10)
 pa.add_argument("--ibdne_flatmeth", type=str, default="none")
+# TODO: update main.nf related to this argument
+pa.add_argument(
+    "--peak_validate_meth", type=str, choices=["xirs", "ihs"], default="xirs"
+)
+# TODO: update main.nf related to this argument
+pa.add_argument(
+    "--ibdne_no_diploid_conversion",
+    type=str,
+    choices=["true", "false"],
+    default="false",
+)
 args = pa.parse_args()
-
 assert args.ibdne_jar is not None
+if args.ibdne_flatmeth is None:
+    args.ibdne_flatmeth == "none"
 assert args.ibdne_flatmeth in ["none", "merge", "keep_hap_1_only"]
-
-
-idx = args.genome_set_id
-ibdne_mincm = args.ibdne_mincm
-ibdne_minregion = args.ibdne_minregion
 
 # output prefix string
 label_str = "_".join(
@@ -40,24 +47,67 @@ label_str = "_".join(
         ]
     ]
 )
+ibdne_mincm = args.ibdne_mincm
+ibdne_minregion = args.ibdne_minregion
 
-# read ibd
-genome = ibdutils.Genome.get_genome("Pf3D7")
-ibd = ibdutils.IBD(genome=genome, label=f"{label_str}_orig")
-ibd.read_ibd(ibd_fn_lst=args.ibd_files)
-ibd.calc_ibd_cov()
-ibd.find_peaks()
+# ------ ibd for all samples
+# create genome obj
+genome_Pf3D7_numeric = ibdutils.Genome.get_genome("Pf3D7")
+# create ibd obj
+ibd = ibdutils.IBD(genome=genome_Pf3D7_numeric, label=f"{label_str}_orig")
+# read ibd from files each containing IBD segment info for a chromosome
+# and remove sample name suffix (such as "~0.9-0.2") that was used
+# for carrying haploid genome ratios
+ibd.read_ibd(ibd_fn_lst=args.ibd_files, rm_sample_name_suffix=False)
+
+# save origin IBD obj for distribution analyses
+of_ibddist_ibdobj = f"{label_str}.ibddist.ibdobj.gz"
+ibd.pickle_dump(of_ibddist_ibdobj)
 
 
-# output files:
-of_ibddist_obj = f"{label_str}.ibddist.ibdobj.gz"
+# save IBD with coverage/peaks for all samples (diploid)
+ibd_all = ibd.duplicate()
+ibd_all.filter_ibd_by_length(min_seg_cm=ibdne_mincm)
+# calculate XiR,s/iHS
+if args.peak_validate_meth == "xirs":
+    ibd_all.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+elif args.peak_validate_meth == "ihs":
+    ibd_all.calc_ihs(vcf_fn_lst=args.vcf_files, min_maf=0.01, multitest_correction=None)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
+# xirs_df = ibd_all.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
 
+if args.ibdne_no_diploid_conversion == "true":
+    print("skip diploid conversion")
+elif args.ibdne_no_diploid_conversion == "false":
+    # convert to heterzygous diploids
+    # Note: remove_hbd might not remove a lot segments as
+    # hdb only involves n/2 pairs of a total of n(n-1)/2 pairs (ratio: 1/(n-1)).
+    # When n is large, the difference is small
+    ibd_all.convert_to_heterozygous_diploids(remove_hbd=True)
 
-# store combined IBD for IBD distribution analysis
-ibd.pickle_dump(of_ibddist_obj)
+    if args.ibdne_flatmeth != "none":
+        ibd_all.flatten_diploid_ibd(method=args.ibdne_flatmeth)
+else:
+    raise NotImplementedError(
+        f"{args.ibdne_no_diploid_conversion} is not valid value is not a value value for option --ibdne_no_diploid_conversion"
+    )
 
+ibd_all.calc_ibd_cov()
+ibd_all.find_peaks()
+# Filter peaks by XiR,s/iHS
+if args.peak_validate_meth == "xirs":
+    xirs_df = ibd_all._xirs_df
+    ibd_all.filter_peaks_by_xirs(xirs_df)
+elif args.peak_validate_meth == "ihs":
+    ibd_all.filter_peaks_by_ihs(min_ihs_hits=1, alpha=0.05)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
+# ibd_all.filter_peaks_by_xirs(xirs_df)
+ibd_all.pickle_dump(f"{label_str}_orig_all.ibdcov.ibdobj.gz")
 
-# make a matrix before possile loading of filterred IBD
+# ------ ibd for samples
+# remove highly related samples
 mat = ibd.make_ibd_matrix()
 
 # if the true IBD passed in, we can run tskibd-ibd filter.
@@ -75,7 +125,7 @@ if len(args.ibd_files_true) != 0:
         run(cmd, shell=True, check=True)
         ibd_files_filtered.append(out)
     # load filterred IBD
-    ibd_filt = ibdutils.IBD(genome=genome, label=f"{label_str}_orig")
+    ibd_filt = ibdutils.IBD(genome=genome_Pf3D7_numeric, label=f"{label_str}_orig")
     ibd_filt.read_ibd(ibd_fn_lst=ibd_files_filtered)
     # replace `_df` member of ibd variable but not the `coverage` and `peaks`
     ibd._df = ibd_filt._df
@@ -84,42 +134,62 @@ if len(args.ibd_files_true) != 0:
 unrelated_samples = ibd.get_unrelated_samples(ibdmat=mat)
 ibd.subset_ibd_by_samples(subset_samples=unrelated_samples)
 
-# ######################################
-# prepare input for IBDNe
-
-#  remove ibd with tmrca < 1.5 (required according to IBDNe paper)
-# this is only useful for tskibd ibd. IBD from other callers have tmrca set to 100
-ibd.filter_ibd_by_time(min_tmrca=1.5)
-
-ibd.filter_ibd_by_length(min_seg_cm=ibdne_mincm)
-
-# calculate XiR,s
-xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
-
-# convert to heterzygous diploids
-# Note: remove_hbd might not remove a lot segments as
-# hdb only involves n/2 pairs of a total of n(n-1)/2 pairs (ratio: 1/(n-1)).
-# When n is large, the difference is small
-ibd.convert_to_heterozygous_diploids(remove_hbd=True)
-
-if args.ibdne_flatmeth != "none":
-    ibd.flatten_diploid_ibd(method=args.ibdne_flatmeth)
-
-# calculate coverage and remove peaks
-ibd.calc_ibd_cov()
-ibd.find_peaks()
-
-ibd.filter_peaks_by_xirs(xirs_df)
+# save ibd coverage/peaks/ibdne for unrelated samples
+ibd_unrel = ibd.duplicate(f"{label_str}_orig")
+ibd_unrel.filter_ibd_by_length(min_seg_cm=ibdne_mincm)
+# NOTE: xirs_df is calculated before converted to diploids
+# calculate XiR,s/iHS
+if args.peak_validate_meth == "xirs":
+    ibd_unrel.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+elif args.peak_validate_meth == "ihs":
+    ibd_unrel.calc_ihs(
+        vcf_fn_lst=args.vcf_files, min_maf=0.01, multitest_correction=None
+    )
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
+# xirs_df = ibd_all.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+# xirs_df = ibd_unrel.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
 
 
-of_orig_ibdne_obj = f"{label_str}_orig.ibdne.ibdobj.gz"
-ibd.pickle_dump(of_orig_ibdne_obj)
+if args.ibdne_no_diploid_conversion == "true":
+    print("skip diploid conversion")
+elif args.ibdne_no_diploid_conversion == "false":
+    # convert to heterzygous diploids
+    # Note: remove_hbd might not remove a lot segments as
+    # hdb only involves n/2 pairs of a total of n(n-1)/2 pairs (ratio: 1/(n-1)).
+    # When n is large, the difference is small
+    ibd_unrel.convert_to_heterozygous_diploids(remove_hbd=True)
 
-ibd2 = ibd.duplicate(f"{label_str}_rmpeaks")
-ibd2.remove_peaks()
-ibd2._df = ibd2.cut_and_split_ibd()
-of_rmpeaks_ibdne_obj = f"{label_str}_rmpeaks.ibdne.ibdobj.gz"
-ibd2.pickle_dump(of_rmpeaks_ibdne_obj)
+    if args.ibdne_flatmeth != "none":
+        ibd_unrel.flatten_diploid_ibd(method=args.ibdne_flatmeth)
+else:
+    raise NotImplementedError(
+        f"{args.ibdne_no_diploid_conversion} is not valid value is not a value value for option --ibdne_no_diploid_conversion"
+    )
+
+ibd_unrel.calc_ibd_cov()
+ibd_unrel.find_peaks()
+# Filter peaks by XiR,s/iHS
+if args.peak_validate_meth == "xirs":
+    xirs_df = ibd_unrel._xirs_df
+    ibd_unrel.filter_peaks_by_xirs(xirs_df)
+elif args.peak_validate_meth == "ihs":
+    ibd_unrel.filter_peaks_by_ihs(min_ihs_hits=1, alpha=0.05)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
+# ibd_unrel.filter_peaks_by_xirs(xirs_df)
+ibd_unrel.pickle_dump(f"{label_str}_orig.ibdne.ibdobj.gz")
+
+# remove peaks and save object for Ne
+ibd_unrel_rmpeaks = ibd_unrel.duplicate(f"{label_str}_rmpeaks")
+ibd_unrel_rmpeaks.remove_peaks()
+# cut and split. Note cut_and_split_ibd does not update self.
+ibd_unrel_rmpeaks._df = ibd_unrel_rmpeaks.cut_and_split_ibd()
+# save of OBJ copy
+ibd_unrel_rmpeaks.pickle_dump(f"{label_str}_rmpeaks.ibdne.ibdobj.gz")
+
+
+# save ibd for ibdne call
 
 # link ibdne.jar file
 if not Path("ibdne.jar").exists():
@@ -130,38 +200,43 @@ if not Path("ibdne.jar").exists():
     print(f"link {this} -> {target}")
 
 # use nerunner (dry_run) to preare inputs and bash scripts
-# --- for ibd before removing ibd peaks ------------------------
+# for ibd before removing ibd peaks ------------------------
 nerunner = ibdne.IbdNeRunner(
-    ibd=ibd,
+    ibd=ibd_unrel,
     input_folder=".",
     output_folder=".",
     mincm=ibdne_mincm,
     minregion=ibdne_minregion,
 )
-nerunner.run(nthreads=10, mem_gb=20, dry_run=True)
+nerunner.run(nthreads=6, mem_gb=20, dry_run=True)
 
-# --- for ibd after removing ibd peaks ------------------------
+
+# ---- save ibd for ibdne call
+# for ibd after removing ibd peaks ------------------------
 nerunner = ibdne.IbdNeRunner(
-    ibd=ibd2,
+    ibd=ibd_unrel_rmpeaks,
     input_folder=".",
     output_folder=".",
     mincm=ibdne_mincm,
     minregion=ibdne_minregion,
 )
-nerunner.run(nthreads=10, mem_gb=20, dry_run=True)
+nerunner.run(nthreads=6, mem_gb=20, dry_run=True)
+
 
 print(
     f"""
       output files:
-        {of_ibddist_obj}
         ibdne.jar
-        {idx}_orig.sh
-        {idx}_orig.map
-        {idx}_orig.ibd.gz
-        {idx}_rmpeaks.sh
-        {idx}_rmpeaks.map
-        {idx}_rmpeaks.ibd.gz
-        {of_orig_ibdne_obj}
-        {of_rmpeaks_ibdne_obj}
-      """
+        {label_str}_orig.sh
+        {label_str}_orig.map
+        {label_str}_orig.ibd.gz
+        {label_str}_orig.cov.pq
+        {label_str}_rmpeaks.sh
+        {label_str}_rmpeaks.map
+        {label_str}_rmpeaks.ibd.gz
+        {label_str}_ibddist.ibdobj.gz
+        {label_str}_orig_all.ibdcov.ibdobj.gz
+        {label_str}_orig.ibdne.ibdobj.gz
+        {label_str}_rmpeaks.ibdne.ibdobj.gz
+    """
 )
--- bin/proc_infomap.py
+++ bin/proc_infomap.py
@@ -8,18 +8,33 @@ pa = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 pa.add_argument("--ibd_files", type=str, nargs=14, required=True)
 pa.add_argument("--vcf_files", type=str, nargs=14, required=True)
 pa.add_argument("--genome_set_id", type=int, required=True)
+
+pa.add_argument(
+    "--peak_validate_meth", type=str, choices=["xirs", "ihs"], default="xirs"
+)
+# Note this option is not used for infomap, only used for preparing input of
+# IBDNe in proc_dist_ne.py
+pa.add_argument(
+    "--ibdne_no_diploid_conversion",
+    type=str,
+    choices=["true", "false"],
+    default="false",
+)
 args = pa.parse_args()
+
+label = args.genome_set_id
 
 
 # read ibd
-genome = ibdutils.Genome.get_genome("Pf3D7")
-ibd = ibdutils.IBD(genome=genome, label="orig")
-ibd.read_ibd(ibd_fn_lst=args.ibd_files)
+genome_Pf3D7_numeric = ibdutils.Genome.get_genome("Pf3D7")
+ibd = ibdutils.IBD(genome=genome_Pf3D7_numeric, label="orig")
+# remove sample name suffix (haploid genome ratio)
+ibd.read_ibd(ibd_fn_lst=args.ibd_files, rm_sample_name_suffix=False)
 
 
 # output files:
-of_ifm_orig_ibd_obj = f"{args.genome_set_id}_orig.ifm.ibdobj.gz"
-of_ifm_rmpeaks_ibd_obj = f"{args.genome_set_id}_rmpeaks.ifm.ibdobj.gz"
+of_ifm_orig_ibdobj = f"{label}_orig.ifm.ibdobj.gz"
+of_ifm_rmpeaks_ibdobj = f"{label}_rmpeaks.ifm.ibdobj.gz"
 
 
 # remove highly relatedness samples
@@ -32,13 +47,22 @@ ibd.subset_ibd_by_samples(subset_samples=unrelated_samples)
 ibd.calc_ibd_cov()
 ibd.find_peaks()
 
-# calculate XiR,s and filter peaks
-xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
-ibd.filter_peaks_by_xirs(xirs_df)
+# calculate XiR,s/iHS
+if args.peak_validate_meth == "xirs":
+    ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+    xirs_df = ibd._xirs_df
+    ibd.filter_peaks_by_xirs(xirs_df)
+elif args.peak_validate_meth == "ihs":
+    ibd.calc_ihs(vcf_fn_lst=args.vcf_files, min_maf=0.01, multitest_correction=None)
+    ibd.filter_peaks_by_ihs(min_ihs_hits=1, alpha=0.05)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
+# xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+# ibd.filter_peaks_by_xirs(xirs_df)
 
 
 ibd2 = ibd.duplicate("rmpeak")
 ibd2.remove_peaks()
 
-ibd.pickle_dump(of_ifm_orig_ibd_obj)
-ibd2.pickle_dump(of_ifm_rmpeaks_ibd_obj)
+ibd.pickle_dump(of_ifm_orig_ibdobj)
+ibd2.pickle_dump(of_ifm_rmpeaks_ibdobj)
